89038905b59905a7b6844b96c8229fc5ccbe4c22
KEM-CONSOLATION <consolationlotachi@gmail.com>
2025-01-11 16:12:27 +0100

    fix

--- src/pages/drops/index.tsx
+++ src/pages/drops/index.tsx
@@ -5,7 +5,7 @@ const Drops = () => {
   return (
     <DashboardLayout>
       <div className="flex justify-center items-center min-h-screen">
-        <p className="text-xl font-bold">Work in Progress</p>
+        <p className="text-xl font-bold">Work in Progress!</p>
       </div>
     </DashboardLayout>
   );
