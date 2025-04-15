961d4bb98c03d6d2b92ee82491623e7df4f25489
Script-Desk <dev@scriptdeskng.com>
2025-04-15 11:44:21 +0100

    fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,21 +5,21 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Studio | Arroweye Pro</title>
-        <meta name="title" content="Studio | Arroweye Pro" />
+        <meta name="title" content="AE Studio | Arroweye Pro" />
         <meta
           name="description"
           content="We empower independent artists and labels with data to grow their revenue."
         />
 
-        <meta property="og:type" content="Studio | Arroweye Pro" />
+        <meta property="og:type" content="AE Studio | Arroweye Pro" />
         <meta property="og:url" content="https://studio.arroweye.pro/" />
-        <meta property="og:title" content="Studio | Arroweye Pro" />
+        <meta property="og:title" content="AE Studio | Arroweye Pro" />
         <meta property="og:description" content="We empower independent artists and labels with data to grow their revenue." />
         <meta property="og:image" content="https://res.cloudinary.com/dih0krdcj/image/upload/v1739434295/Arroweye%20Pro/gprcom6jx4iunr6jyu7o.jpg" />
 
-        <meta property="twitter:card" content="Studio | Arroweye Pro" />
+        <meta property="twitter:card" content="AE Studio | Arroweye Pro" />
         <meta property="twitter:url" content="https://studio.arroweye.pro/" />
-        <meta property="twitter:title" content="Studio | Arroweye Pro" />
+        <meta property="twitter:title" content="AE Studio | Arroweye Pro" />
         <meta property="twitter:description" content="We empower independent artists and labels with data to grow their revenue." />
         <meta property="twitter:image" content="https://res.cloudinary.com/dih0krdcj/image/upload/v1739434295/Arroweye%20Pro/gprcom6jx4iunr6jyu7o.jpg" />
 
